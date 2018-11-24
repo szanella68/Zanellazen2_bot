c1cab98616f6376c7ab1f6bf43aca8ff751633b4
szanella68 <45138935+szanella68@users.noreply.github.com>
2018-11-24 09:52:37 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+?php
+  include 'Token.php';
+  $update = file_get_contents('php://input');
+  $update = json_decode($update, TRUE);
+  
+  $chatId = $update['message']['from']['id'];
+  $nome = $update['message']['from']['first_name'];
+  $text = $update['message']['text'];
+  $query = $update['callback_query'];
+  
+   if(strpos($text,"+")!==false){
+          sendMessage($chatId,eval('return '.$text.';'));
+         exit();
+   }
+   
+     function sendMessage($chatId,$text){
+      $url = $GLOBALS[website]."/sendMessage?chat_id=$chatId&text=".urlencode($text);
+      file_get_contents($url);
+     }
+     
+ ?>
+   
