ce057c2ec6988d4e88c8a6cac7050f0e92c0095f
szanella68 <45138935+szanella68@users.noreply.github.com>
2018-11-24 10:04:15 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,5 +1,5 @@
 ?php
-  $botToken = "TOKEN DEL BOT";
+  $botToken = "681369794:AAGuUAUjHDGAOsmo3GXX7VSdO9E5-OTISYE";
   $website = "https://api.telegram.org/bot".$botToken;
   $update = file_get_contents('php://input');
   $update = json_decode($update, TRUE);
