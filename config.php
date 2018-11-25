62293950d61d42002dcdd5c7d2d9671077c50055
szanella68 <45138935+szanella68@users.noreply.github.com>
2018-11-25 17:31:50 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	$botToken = "681369794:AAGuUAUjHDGAOsmo3GXX7VSdO9E5-OTISYE";
+	$website = "https://api.telegram.org/bot".$botToken; 
+	mysql_connect('localhost', 'zanellazen', 'silvia12!');
+	mysql_select_db('my_zanellazen'); 
+	//https://api.telegram.org/bot681369794:AAGuUAUjHDGAOsmo3GXX7VSdO9E5-OTISYE/setwebhook?url=https://zanellazen.altervista.org/zanellazenbot/zan2/execute.php
+?>
